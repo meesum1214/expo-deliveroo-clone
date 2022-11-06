a541588cc74c0fe11ec3296b316289ece9a410b8
salman1214 <salmannaqvi461@gmail.com>
2022-11-06 07:12:06 -0800

    expo build

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import React, { useEffect } from 'react'
 import * as Animatable from 'react-native-animatable'
 import * as Progress from 'react-native-progress';
